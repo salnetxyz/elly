818e368ccd5809b34c60082628678bfd841d566e
sahilnetic <jsthil99@gmail.com>
2023-02-13 02:22:06 +0530

    fixed minor bug

--- pages/components/TemplateCard.js
+++ pages/components/TemplateCard.js
@@ -2,9 +2,9 @@ import Image from "next/image"
 import Link from "next/link"
 const TemplateCard = ({image})=>{
    return(
-    <div className="hover:opacity-95 hover:brightness-50 border border-black border-l-2 rounded-lg border-b-4">
+    <div className="hover:opacity-95 hover:brightness-50">
     <Image
-    className="rounded-lg"
+    className="rounded-lg  border border-black border-l-2 rounded-lg border-b-4"
     src={`/${image}`}
     width={600}
     height={600}
