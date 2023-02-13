a946d16c309462241d941237baed8e1c850faa61
sahilnetic <jsthil99@gmail.com>
2023-02-13 13:29:42 +0530

    added demo

--- pages/components/TemplateCard.js
+++ pages/components/TemplateCard.js
@@ -1,14 +1,16 @@
 import Image from "next/image"
 import Link from "next/link"
-const TemplateCard = ({image})=>{
+const TemplateCard = ({image, name})=>{
    return(
     <div className="hover:opacity-95 hover:brightness-50">
+    <div className="rounded-lg  border border-black border-l-2 rounded-lg border-b-4">
     <Image
-    className="rounded-lg  border border-black border-l-2 rounded-lg border-b-4"
     src={`/${image}`}
     width={600}
     height={600}
     />
+    <p className="py-8 brightness-125 shadow-xl bg-white text-sky-600 opacity-95 text-center">{name}</p>
+   </div>
  </div>
    )
 }
--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,20 @@ const Home = () => {
     </Link>
     </div>
 
-    <div className="flex-none">
+    {/* <div className="flex-none">
     <Link href="/" className="text-sm font-semibold pl-3 sm:pl-6 rounded-full py-1 text-white flex gap-1">
     <svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
+    </svg>
+    </Link>
+    </div> */}
+
+
+    <div className="flex-none">
+    <Link href="/" className="text-sm font-semibold pl-3 sm:pl-6 rounded-full py-1 text-white flex gap-1">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="w-6 h-6">
+    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
     </svg>
     </Link>
     </div>
